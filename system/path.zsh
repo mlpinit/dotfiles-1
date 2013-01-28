cb0c80bf1e1acbb8ba3987ed91015398abacd9f9
slnovak <stefan.louis.novak@gmail.com>
2013-01-28 08:59:15 -0500

    update system path and node config

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:./node_modules/.bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/local/heroku/bin:$HOME/.rvm/bin:/usr/local/share/npm/bin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

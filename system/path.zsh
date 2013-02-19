adb075a9e7298fa058d52b7b0f2b954ac981a0c0
Stefan Novak <stefan.louis.novak@gmail.com>
2013-02-18 21:43:36 -0500

    add cabal to path, remove edit-server

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="$HOME/.local/bin:./bin:./node_modules/.bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/local/heroku/bin:$HOME/.rvm/bin:/usr/local/share/npm/bin:$HOME/.sfs:$ZSH/bin:$PATH:/usr/local/share/python"
+export PATH="$HOME/.local/bin:./bin:./node_modules/.bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/local/heroku/bin:$HOME/.rvm/bin:/usr/local/share/npm/bin:$HOME/.sfs:$ZSH/bin:$PATH:/usr/local/share/python:$HOME/.cabal/bin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

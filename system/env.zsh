ed30c4e90575ff8ab4cd01ffc5d4272344772b62
Stefan Novak <stefan.louis.novak@gmail.com>
2013-08-01 09:20:53 -0400

    update env

--- system/env.zsh
+++ system/env.zsh
@@ -1,6 +1,2 @@
 export EDITOR='vim'
 export PAGER='less'
-
-export WORKON_HOME=$HOME/.virtualenvs
-
-[[ -f /usr/local/share/python/virtualenvwrapper.sh ]] && source /usr/local/share/python/virtualenvwrapper.sh

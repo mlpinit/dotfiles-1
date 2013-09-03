88860740c1c2bc1439c5a4373c9c3abe38e8d77d
Stefan Novak <stefan.louis.novak@gmail.com>
2013-09-03 10:09:24 -0400

    update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias task_sync='cd ~/gists/fedbaceddf11a643050f && task all > tasks && git add tasks && git commit -m "update tasks" && git push'
+alias marius_tasks='curl https://gist.github.com/mlpinit/533f2d35980e866fc5cd/raw/a6524bd344f381b7a3324735f694e415401f4eb2/tasks'
+alias be='bundle exec'
